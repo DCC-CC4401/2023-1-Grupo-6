b8f584458a64d6370f32f967fea652cdb56e7c9d
SeaMira <sebastianandresmira@gmail.com>
2023-04-27 17:48:35 -0400

    feat: added new models for Reviews, Comments and ReviewsxUser.

--- group_6_project/reviews/models.py
+++ group_6_project/reviews/models.py
@@ -1,3 +1,4 @@
+from django.utils import timezone
 from django.db import models
 
 # Create your models here.
@@ -9,3 +10,34 @@ class User(AbstractUser):
   # pais = models.CharField(max_length=5,choices=paises)
   apodo = models.CharField(max_length=30)
     
+
+class Review(models.Model):  # Todolist able name that inherits models.Model
+    # categoria = models.ForeignKey(categories, default="general", on_delete=models.CASCADE)  
+    nombre_producto = models.CharField(max_length=100)# autodesc
+    contenido =	models.TextField(max_length=500)# donde se obtuvo, es un alimento, herramienta, etc. como se utiliza, precio
+    fecha_creacion = models.DateField(default=timezone.now().strftime("%Y-%m-%d"))# autodesc
+    CHOICES = [
+       ("ONE_STAR", 1),
+       ("TWO_STARS", 2),
+       ("THREE_STARS", 3),
+       ("FOUR_STARS", 4),
+       ("FIVE_STARS", 5),
+    ]
+    puntuacion = models.IntegerField(choices=CHOICES)# el conjunto de todas las ponderaciones dada la cantidad de valoraciones y el clasificador elegido
+    likes = models.IntegerField(null=True, default=0)
+    dislikes = models.IntegerField(null=True, default=0)
+    cantidad_votantes =	models.IntegerField(null=True, default=0)# contador de la cantidad de reseñas
+    nick_autor =  models.ForeignKey(User, default="general", on_delete=models.CASCADE)# autodesc
+
+
+class Reviews_Username(models.Model):
+   id_reseña = models.ForeignKey(Review, default="general", on_delete=models.CASCADE)
+   username = models.ForeignKey(User, default="general", on_delete=models.CASCADE)
+
+class Comments(models.Model):
+    id_reseña = models.ForeignKey(Review, default="general", on_delete=models.CASCADE)
+    comentador = models.ForeignKey(User, default="general", on_delete=models.CASCADE)
+    fecha_creacion = models.DateField(default=timezone.now().strftime("%Y-%m-%d"))# autodesc
+
+
+   
