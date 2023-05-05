8c7474bc38096ea7842cc523728218bf6e3454c2
SeaMira <sebastian.mira@ug.uchile.cl>
2023-05-05 19:51:01 -0400

    feat: can log in with a created User.

--- group_6_project/group_6_project/settings.py
+++ group_6_project/group_6_project/settings.py
@@ -82,6 +82,10 @@ DATABASES = {
 }
 
 
+LOGIN_URL = '/login/'
+
+LOGOUT_URL = '/createReview/'
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
--- group_6_project/reviews/views.py
+++ group_6_project/reviews/views.py
@@ -1,17 +1,22 @@
-from django.shortcuts import render
-from .forms import LogInForm, RegisterForm
+from django.shortcuts import render, redirect
+from .forms import LogInForm, RegisterForm, CreateReviewForm
 from reviews.models import User
 from django.http import HttpResponseRedirect
-from django.contrib.auth import authenticate
-from .forms import LogInForm, CreateReviewForm
+from django.contrib.auth import authenticate, login, logout
 # Create your views here.
 
 def Login(request):
     if request.method == 'POST':
         form = LogInForm(request.POST)
         if form.is_valid():
-            # TODO: authenticate user and redirect to home page
-            pass
+            username = form.cleaned_data['nick_name']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('/createReview/')
+            else:
+                form.add_error(None, 'Invalid username or password')
     else:
         form = LogInForm()
     return render(request, 'reviews/LogIn.html', {'form': form})
